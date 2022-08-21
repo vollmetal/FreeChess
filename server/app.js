ce549e254074600cb024523550910e63ad38c002
vollmetal <sarah@avins.org>
2022-08-21 16:46:08 -0400

    something

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const http = require('http');
+const http = require('https');
 const server = http.createServer(app);
 global.mongoose = require('mongoose')
 global.bcrypt = require('bcryptjs')
